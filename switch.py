a3be9be38cee0b6d1ace0be2b154b6af5b89fc9e
Giovanni <masteruan@gmail.com>
2017-07-25 15:44:41 +0200

    Create switch.py

---
+++ switch.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time import os
+buttonPin = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+last_state = True
+
+input_state = True
+
+while True:
+  input_state = GPIO.input(buttonPin)
+
+  if (not input_state):
+    print("Shutdown")
+    os.system('sudo shutdown -h now')
+    time.sleep(0.05)
+    GPIO.cleanup()
+
